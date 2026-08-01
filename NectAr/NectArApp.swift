cd93e6c23706603af3b019467d6a0696ed7fe408
AbrMac <abraarjh@gmail.com>
2026-08-01 14:04:51 +0700

    Initial Commit

---
+++ NectAr/NectArApp.swift
@@ -0,0 +1,17 @@
+//
+//  NectArApp.swift
+//  NectAr
+//
+//  Created by abr on 01/08/26.
+//
+
+import SwiftUI
+
+@main
+struct NectArApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
