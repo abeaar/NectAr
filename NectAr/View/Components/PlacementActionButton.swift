84af77c7fc25f843f10d464ca94423bc07001a82
Zee <putriazizamv@gmail.com>
2026-08-06 16:58:05 +0700

    add component placement action button

---
+++ NectAr/View/Components/PlacementActionButton.swift
@@ -0,0 +1,51 @@
+//
+//  PlacementActionButton.swift
+//  NectAr
+//
+//  Created by Putri Aziza Mufva on 06/08/26.
+//
+
+import SwiftUI
+
+struct PlacementActionButton: View {
+    @State var isComplete: Bool = false
+    @State var isUndo: Bool = false
+    @State var isRedo: Bool = false
+    
+    var body: some View {
+        HStack {
+            VStack(spacing: 14) {
+                Button(action: {
+                    self.isRedo.toggle()
+                }) {
+                    Image(systemName: "arrow.uturn.forward.circle.fill")
+                        .font(.system(size: 50))
+                        .foregroundColor(.white.opacity(0.9))
+                }
+                
+                Button(action: {
+                    self.isUndo.toggle()
+                }) {
+                    Image(systemName: "arrow.uturn.backward.circle.fill")
+                        .font(.system(size: 50))
+                        .foregroundColor(.white.opacity(0.9))
+                        .padding(.bottom, 10)
+                }
+                
+                Button(action: {
+                    self.isComplete.toggle()
+                }) {
+                    Image(systemName: isComplete ? "play.circle.fill" : "plus.circle.fill")
+                        .font(.system(size: 85))
+                        .foregroundColor(.white.opacity(0.9))
+                }
+            }
+        }
+// for dev only
+//        .background(.black)
+    }
+}
+
+#Preview {
+    PlacementActionButton()
+}
