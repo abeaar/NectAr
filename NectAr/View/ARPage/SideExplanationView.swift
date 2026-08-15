4afcbacbe5e5ee64cb6fd929ea4d98226c9bbc00
Zee <putriazizamv@gmail.com>
2026-08-16 00:53:05 +0700

    fix: [ZEE] fix visibility button's position

--- NectAr/View/ARPage/SideExplanationView.swift
+++ NectAr/View/ARPage/SideExplanationView.swift
@@ -12,7 +12,33 @@ struct ExplanationView: View {
     @State private var viewModel = ExpSimulationViewModel()
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 14) {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 14) {
+                if viewModel.isListVisible {
+                    ScrollView(.vertical, showsIndicators: false){
+                        VStack(spacing: 14) {
+                            ForEach(viewModel.cards) { card in
+                                ExplanationCard(title: card.title, description: card.description)
+                                    .opacity(viewModel.activeCardID == card.id ? 1.0 : 0.4)
+                                    .id(card.id)
+                                    .onTapGesture {
+                                        withAnimation(.easeInOut) {
+                                            viewModel.setActiveCard(id: card.id)
+                                        }
+                                    }
+                            }
+                        }
+                        .padding(.leading, 30)
+                        .scrollTargetLayout()
+                    }
+                    .frame(width: 350)
+                    .scrollPosition(id: $viewModel.activeCardID, anchor: .center)
+                    .contentMargins(.bottom, 700, for: .scrollContent)
+                    .scrollTargetBehavior(.viewAligned)
+                    .transition(.move(edge: .leading).combined(with: .opacity))
+                }
+            }
+            
             Button(action: {
                 withAnimation(.easeInOut) {
                     viewModel.toggleVisibility()
@@ -23,35 +49,12 @@ struct ExplanationView: View {
                     .scaledToFit()
                     .frame(width: 64)
             }
-            .padding(.leading, 26)
+            .padding(.leading, 20)
             .padding(.top, 10)
-            
-            if viewModel.isListVisible {
-                ScrollView(.vertical, showsIndicators: false){
-                    VStack(spacing: 14) {
-                        ForEach(viewModel.cards) { card in
-                            ExplanationCard(title: card.title, description: card.description)
-                                .opacity(viewModel.activeCardID == card.id ? 1.0 : 0.4)
-                                .id(card.id)
-                                .onTapGesture {
-                                    withAnimation(.easeInOut) {
-                                        viewModel.setActiveCard(id: card.id)
-                                    }
-                                }
-                        }
-                    }
-                    .padding(.leading, 30)
-                    .scrollTargetLayout()
-                }
-                .frame(width: 350)
-                .scrollPosition(id: $viewModel.activeCardID, anchor: .center)
-                .contentMargins(.bottom, 500, for: .scrollContent)
-                .scrollTargetBehavior(.viewAligned)
-                .transition(.move(edge: .leading).combined(with: .opacity))
-            }
+
             Spacer()
         }
-        .frame(maxWidth: .infinity, alignment: .leading)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 
