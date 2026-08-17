f744b6824c933009517ba06d1f0046dec9dc7c80
Zee <putriazizamv@gmail.com>
2026-08-17 21:10:18 +0700

    feat: [ZEE] add background on menu view

--- NectAr/View/MenuPage/MenuView.swift
+++ NectAr/View/MenuPage/MenuView.swift
@@ -25,7 +25,6 @@ struct MenuView: View {
                 
                 Image("MenuPageBG")
                     .resizable()
-                    .scaledToFit()
                     .ignoresSafeArea()
                 
                 VStack (spacing: 70) {
