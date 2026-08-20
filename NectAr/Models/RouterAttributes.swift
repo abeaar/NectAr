38e9a67eb97061c06417b44ffdba7f3ad8afd156
benaya <benayagiven789@gmail.com>
2026-08-20 11:48:40 +0700

    chore: [BGJ] tune default router range and add locked button asset

--- NectAr/Models/RouterAttributes.swift
+++ NectAr/Models/RouterAttributes.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct RouterAttributes: Equatable {
     var isOn: Bool = true
-    var range: Float = 10.0
+    var range: Float = 1.0
 
     var maxConnectedDevices: Int = 5
     var frequencyBand: FrequencyBand = .band5GHz
